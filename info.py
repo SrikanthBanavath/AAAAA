6b5f6cf68f5d9c6321c288297b70eb7aa93f4a5e
Srikanth Nayak <151753362+BanavathSrikanth@users.noreply.github.com>
2024-07-25 21:18:48 +0530

    Update info.py

--- info.py
+++ info.py
@@ -143,7 +143,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "")
+URL = environ.get("URL", "https://aesthetic-ceil-alx-martin-897e9591.koyeb.app/")
 
 
 # Rename Info : If True Then Bot Rename File Else Not
