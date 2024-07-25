4a6926dbd4ac5ec84399eff6de25d59edb83b2b6
Srikanth Nayak <151753362+BanavathSrikanth@users.noreply.github.com>
2024-07-26 00:05:05 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -228,9 +228,11 @@ Nᴀᴍᴇ - {}"""
 
 𝗠𝗲𝘀𝘀𝗮𝗴𝗲 <b>: {}</b>"""
 
-    CAPTION = """<b>📂Fɪʟᴇɴᴀᴍᴇ : {file_name}
+    CAPTION = """<b>{file_caption}
 
-<b> Size ⚙️: {file_size}</b>""" 
+Size : {file_size}
+
+Join : @CinemasMawa😏</b>""" 
 
     IMDB_TEMPLATE_TXT = """
 <b>Query: {qurey}
