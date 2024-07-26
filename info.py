8aa1d83da1fc8fdbbc9147870b645b3cff5eac5c
Srikanth Nayak <151753362+BanavathSrikanth@users.noreply.github.com>
2024-07-26 22:03:56 +0530

    Update info.py

--- info.py
+++ info.py
@@ -143,7 +143,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "https://psb-vj.onrender.com/")
+URL = environ.get("URL", "https://psb-nzli.onrender.com/")
 
 
 # Rename Info : If True Then Bot Rename File Else Not
